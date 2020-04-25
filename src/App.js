cb397d740358d8b55d60c0ade9c6d8e3eb558b33
tsepa0 <tsepa.stepan@gmail.com>
2020-04-25 15:25:02 +0300

    Todos as cards using bootstrap styles

--- src/App.js
+++ src/App.js
@@ -2,10 +2,66 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import "bootstrap/dist/css/bootstrap.css";
+import { Card } from "react-bootstrap";
+
 import $ from 'jquery'
 
-function TodoItem(args) {
-    return <li><input type="checkbox" checked={args.isCompleted} />{args.children}</li>
+class TodoItem extends Component {
+    constructor(args) {
+        super(args);
+
+        this.id = args.id
+        this.title = args.name
+        this.task = args.task
+
+        this.state = {
+            isCompleted: args.isCompleted
+        }
+
+        this.toggleButtonId =  `toggleBtn${this.id}`
+
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    toggleButtonStyle() {
+        const btn = $(document.getElementById(this.toggleButtonId))
+
+        const classActive = 'btn-success'
+        const classDisabled = ''
+
+        if (btn.hasClass(classActive)) {
+            btn.removeClass(classActive)
+            btn.addClass(classDisabled)
+        } else {
+            btn.removeClass(classDisabled)
+            btn.addClass(classActive)
+        }
+    }
+
+    handleClick() {
+        this.toggleButtonStyle()
+        this.setState({ isCompleted: !this.state.isCompleted })
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="card text-left"  style={{ width: '20rem'}}>
+                    <div className="card-header card-title">{this.title}</div>
+                    <div className="card-body">
+                        <p className="card-text">{this.task}</p>
+                        <hr/>
+                        <div id={this.toggleButtonId} onClick={this.handleClick} className="btn toggle-btn">
+                            Single toggle <input className="toggle-checkbox" readOnly={true} type="checkbox" checked={this.state.isCompleted} />
+                        </div>
+                    </div>
+                </div>
+                <p/>
+            </div>
+
+        )
+    }
 }
 
 class App extends Component {
@@ -17,16 +73,16 @@ class App extends Component {
 
         this.state = {
             cards: [ {
-                name: 'AAA',
-                task: 'Do smth...',
+                title: 'AAA',
+                description: 'Do smth...',
                 completed: false
             }, {
-                name: 'bbb',
-                task: 'Do more!',
+                title: 'bbb',
+                description: 'Do more!',
                 completed: false
             }, {
-                name: 'CCC',
-                task: 'Do not do anything',
+                title: 'CCC',
+                description: 'Do not do anything',
                 completed: false
             } ]
         };
@@ -36,10 +92,7 @@ class App extends Component {
     }
 
     handleChange(event) {
-        const newInput = event.target.value;
-
-        const mainText = $(document.getElementById(this.textId))
-        mainText.text(this.textToShow(newInput))
+        // const newInput = event.target.value;
     }
 
     render() {
@@ -57,15 +110,17 @@ class App extends Component {
                 </div>
                 <div className="App-header">
                     <img className="App-logo" id={this.logoId} src={logo} alt="logo" />
-                    <ul>
-                        {this.state.cards.map((card, index) => {
-                            return <TodoItem
-                                key={index}
-                                isCompleted={card.completed}>
-                                {card.task}
-                            </TodoItem>
-                        })}
-                    </ul>
+                        <ul className="list-group list-group-flush">
+                            {this.state.cards.map((card, index) => {
+                                return <TodoItem
+                                    key={index}
+                                    id={index}
+                                    name={card.title}
+                                    task={card.description}
+                                    isCompleted={card.completed}>
+                                </TodoItem>
+                            })}
+                        </ul>
                 </div>
             </div>
         );
