6dd607f11c5a1379ea7579cb0868cc3505751b7b
tsepa0 <tsepa.stepan@gmail.com>
2020-05-08 02:00:15 +0300

    Remove jquery usage with native document object

--- src/Card.js
+++ src/Card.js
@@ -1,35 +1,32 @@
-import React, {Component} from "react";
-import $ from "jquery";
+import React from "react";
 
-import s from './Card.module.css'
-
-//
-// let state = {
-//     isCompleted: args.isCompleted
-// }
-
-// handleClick = handleClick.bind(this)
+// import s from './Card.module.css'
 
 const Card = (props) => {
     const toggleButtonStyle = () => {
-        const btn = $(document.getElementById(`toggleBtn${props.id}`))
+        const btn = document.getElementsByClassName('btn').item(props.id)
 
         const classActive = 'btn-success'
-        const classDisabled = ''
-
-        if (btn.hasClass(classActive)) {
-            btn.removeClass(classActive)
-            btn.addClass(classDisabled)
+        if (btn.classList.contains(classActive)) {
+            btn.classList.remove(classActive)
         } else {
-            btn.removeClass(classDisabled)
-            btn.addClass(classActive)
+            btn.classList.add(classActive)
         }
     }
 
     const handleClick = () => {
         toggleButtonStyle()
         props.state.isCompleted = !props.state.isCompleted
-        console.log(props.state.isCompleted)
+
+        const toggle = document.getElementsByClassName('toggle-checkbox').item(props.id)
+        if (props.state.isCompleted) {
+            toggle.setAttribute('checked', '')
+        } else {
+            toggle.removeAttribute('checked')
+        }
+        // toggle.setAttribute('checked', props.state.isCompleted)
+
+        console.log(toggle.attributes)
     }
 
     return (
@@ -39,12 +36,9 @@ const Card = (props) => {
                 <div className="card-body">
                     <p className="card-text">{props.task}</p>
                     <hr/>
-                    <div id={`toggleBtn${props.id}`}
-                         onClick={handleClick}
-                         className="btn toggle-btn">
-
-                        Single toggle <input className="toggle-checkbox" readOnly={false} type="checkbox"
-                                             checked={props.state.isCompleted}/>
+                    <div onClick={handleClick} className="btn toggle-btn">
+                        Single toggle
+                        <input className="toggle-checkbox" readOnly={false} type="checkbox"/>
                     </div>
                 </div>
             </div>
