4bf622e92c259d297d036a812bc464c3656d87e6
tsepa0 <tsepadev@gmail.com>
2020-05-14 16:18:01 +0300

    Get args instead of props.args as params

--- src/components/Info/CountryInfo.js
+++ src/components/Info/CountryInfo.js
@@ -1,33 +1,29 @@
 import React from "react";
 
-const CountryInfo = (props) => {
+const CountryInfo = ({args}) => {
 
-    if (props.args.error) {
-        return (
-            <div>
-                {props.args.error}
-            </div>
-        )
+    if (args.error) {
+        return (<div>{args.error}</div>)
     }
 
     return (
-        <div className='card'>
-            <div className='card-header'>{props.args.country}, {props.args.continent}</div>
-            <div className='card-body hidden'>
-                <div>active: {props.args.active}</div>
-                <div>cases: {props.args.cases}</div>
-                <div>casesPerOneMillion: {props.args.casesPerOneMillion}</div>
-                <div>critical: {props.args.critical}</div>
-                <div>deaths: {props.args.deaths}</div>
-                <div>deathsPerOneMillion: {props.args.deathsPerOneMillion}</div>
-                <div>recovered: {props.args.recovered}</div>
+        <div>
+            <div>{args.country}, {args.continent}</div>
+            <div className='hidden info-content'>
+                <div>active: {args.active}</div>
+                <div>cases: {args.cases}</div>
+                <div>casesPerOneMillion: {args.casesPerOneMillion}</div>
+                <div>critical: {args.critical}</div>
+                <div>deaths: {args.deaths}</div>
+                <div>deathsPerOneMillion: {args.deathsPerOneMillion}</div>
+                <div>recovered: {args.recovered}</div>
                 <hr/>
-                <div>tests: {props.args.tests}</div>
-                <div>testsPerOneMillion: {props.args.testsPerOneMillion}</div>
+                <div>tests: {args.tests}</div>
+                <div>testsPerOneMillion: {args.testsPerOneMillion}</div>
                 <hr/>
-                <div>todayCases: {props.args.todayCases}</div>
-                <div>todayDeaths: {props.args.todayDeaths}</div>
-                <div>updated: {props.args.updated}</div>
+                <div>todayCases: {args.todayCases}</div>
+                <div>todayDeaths: {args.todayDeaths}</div>
+                <div>updated: {args.updated}</div>
             </div>
         </div>
 
